f7842594009122974e1b7f7a29a9ea77395b43dd
Adam Barton <firepoisonx@gmail.com>
2019-10-26 01:17:51 -0500

    Adding base scaffolding for our project, including standard .gitignore

---
+++ StringCalculator.Impl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace challenge_calculator
+{
+    class StringCalculatorMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
